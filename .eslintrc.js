c7da7764a405841363c7bac4e8237cb815da17fb
samseburn <90seok_0@naver.com>
2022-11-21 12:29:08 +0900

    🔧 eslint 및 prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'no-unused-vars': 1,
+    'no-console': 0,
+  },
+};
