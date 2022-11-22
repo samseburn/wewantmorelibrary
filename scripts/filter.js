5c7dfc642e2eee5801bd0a599cd48c85474bcbde
samseburn <90seok_0@naver.com>
2022-11-22 21:29:13 +0900

    ✨ 도서관 필터 버튼 기능 구현

---
+++ scripts/filter.js
@@ -0,0 +1,52 @@
+// 필터 버튼 기능
+const filterCategoryBtns = document.querySelectorAll('.filter-category-item');
+const all = document.querySelector('.all');
+const guList = document.querySelector('.gu-list');
+const filterBtnList = document.querySelector('.filter-list');
+const filterBtns = document.querySelectorAll('.filter-item');
+const placeholder = document.querySelector('.placeholder');
+
+// 전체
+all.addEventListener('click', function (e) {
+  switchBtn(guList);
+  all.classList.toggle('is-active');
+  if (all.classList.contains('is-active')) {
+    filterBtnList.classList.remove('is-shown');
+  }
+});
+
+// 구별보기
+guList.addEventListener('click', function (e) {
+  removeClass(filterBtns);
+  switchBtn(all);
+  guList.classList.toggle('is-active');
+
+  if (!guList.classList.contains('is-active')) {
+    filterBtnList.classList.remove('is-shown');
+    placeholder.classList.remove('hide');
+  } else {
+    filterBtnList.classList.toggle('is-shown');
+  }
+});
+
+filterBtns.forEach((button) => {
+  button.addEventListener('click', function (e) {
+    removeClass(filterBtns);
+    button.classList.toggle('is-active');
+  });
+});
+
+function removeClass(btns) {
+  btns.forEach((button) => {
+    if (button.classList.contains('is-active')) {
+      button.classList.remove('is-active');
+    }
+    // console.log(button);
+  });
+}
+
+function switchBtn(btn) {
+  if (btn.classList.contains('is-active')) {
+    btn.classList.remove('is-active');
+  }
+}
